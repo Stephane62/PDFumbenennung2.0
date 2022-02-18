8cd1197093c5ca988d0cb6ba98ef4804b7c1f8bd
Emre Oztas <erme431@gmail.com>
2022-02-18 21:56:11 +0300

    project started

---
+++ Reader.py
@@ -0,0 +1,21 @@
+from PyPDF2 import PdfFileReader
+import os
+
+def main():
+    print("main")
+
+
+def getInfos(text):
+    print("infos")
+
+
+def changeFilename(name):
+    print("changeName")
+
+
+def checkIfKey(info):
+    print("check")
+
+
+if __name__ == '__main__':
+    main()
